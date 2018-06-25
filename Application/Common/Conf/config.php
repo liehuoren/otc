add6dede8f6a8addf602c9f3d9e74f17c232d46d
烈火人 <admin@zhlzzz.com>
2018-06-25 17:19:14 +0800

    去除多余文件

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -10,10 +10,10 @@ return array(
     'DB_HOST'               =>  'rm-j6c5zlcc582dy40sbzo.mysql.rds.aliyuncs.com', // 服务器地址
 //    'DB_HOST'               =>  '192.168.1.113', // 服务器地址
     'DB_NAME'               =>  'speetotc',          // 数据库名
-    'DB_USER'               =>  'chuangkeroot',      // 用户名
-    'DB_PWD'                =>  '$Chuangke542397',          // 密码
-//    'DB_USER'               =>  'root',      // 用户名
-//    'DB_PWD'                =>  'root',          // 密码
+    // 'DB_USER'               =>  'chuangkeroot',      // 用户名
+    // 'DB_PWD'                =>  '$Chuangke542397',          // 密码
+   'DB_USER'               =>  'root',      // 用户名
+   'DB_PWD'                =>  '123456',          // 密码
     'DB_PORT'               =>  '3306',        // 端口
     'DB_PREFIX'             =>  'trade_',    // 数据库表前缀
     
